849f23bd3ac73dba2f0d8b1a6bb5060f364fc58d
unknown <iskandar.django.work@gmail.com>
2026-05-21 17:20:08 +0500

    refactor(websocket-architecture): add chat store for managing state

--- backend/src/types/client.ts
+++ backend/src/types/client.ts
@@ -1,9 +1,22 @@
-export type ClientFrontendData = {
-  my: ClientPersonalData;
-  search: ClientPersonalData;
-};
+// export type ClientFrontendData = {
+//   my: ClientPersonalData;
+//   search: ClientPersonalData;
+// };
 
 export type ClientPersonalData = {
   sex: "m" | "w";
   age: "below 17" | "17-25" | "25-36" | "over 36";
 };
+
+export type ClientFrontendData =
+  | {
+      type: "SEND";
+      data: { message: string };
+    }
+  | {
+      type: "MATCH";
+      data: ClientPersonalData;
+    }
+  | {
+      type: "LEAVE";
+    };
